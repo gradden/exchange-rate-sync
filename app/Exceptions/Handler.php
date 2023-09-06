437a0d16e2d8937ed05f336a8e4b14c70b8b848f
Tóth Gergő <tgery3@gmail.com>
2023-09-06 22:33:28 +0200

    Changed error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -19,11 +20,15 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            return $this->custom($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR, $e);
         });
 
         $this->renderable(function (Throwable $e) {
-            return $this->custom($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+            if ($e instanceof NotFoundHttpException) {
+                return response()->view('vendor.backpack.ui.errors.404', ['exception' => $e]);
+            }
+
+            return response()->view('vendor.backpack.ui.errors.500', ['exception' => $e]);
         });
     }
 }
